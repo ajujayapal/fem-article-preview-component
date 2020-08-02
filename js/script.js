9add8e1ffa708155585569d0180d16bb210f7169
Aju Jayapalan <aju.jayapal@gmail.com>
2020-08-01 23:18:21 -0500

    First Commit

---
+++ js/script.js
@@ -0,0 +1,12 @@
+const divFooter1 = document.querySelector("#footer-1");
+const divFooter2 = document.querySelector("#footer-2");
+const triggerFooter1 = document.querySelector("#footer-1 .social-link");
+const triggerFooter2 = document.querySelector("#footer-2 .social-link");
+
+triggerFooter1.addEventListener("click", toggleFooter);
+triggerFooter2.addEventListener("click", toggleFooter);
+
+function toggleFooter() {
+  divFooter2.classList.toggle("display-social");
+  divFooter2.classList.toggle("hide-social");
+}
